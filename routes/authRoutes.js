9599f3991e3995978220cd2deb0840e46bc16aac
moeedasger14-lab <moeedasger14@gmail.com>
2026-01-22 22:38:53 +0500

    kxucyet

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const User  = reqiure("../modals/user");
 // ✅ destructure functions
 const { signup, getMe } = require("../controllers/authController");
+// GET logged-in user profile
+router.get("/me", protect, async (req, res) => {
+  const user = await User.findById(req.user._id).select(
+    "fullName gender teacherProfile role status"
+  );
 
+  res.json(user);
+});
 // routes
 router.post("/signup", signup);
 router.get("/me", getMe);
